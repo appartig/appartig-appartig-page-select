a5c409a76081c6d7c0ed6f637947c0aefd4783e1
Jakob Vesely <jakob@appartig.at>
2020-04-13 13:35:44 +0200

    Init Version

---
+++ appartig_page_select.php
@@ -0,0 +1,146 @@
+<?php
+	
+	/*
+		Plugin Name: AppArtig Page Select
+		Description: Plugin for 
+		Version:     1.0.0
+		Author:      AppArtig e.U.
+		Author URI:  https://www.appartig.at
+		License:     APPARTIG/AGB
+		License URI: https://www.appartig.at/agb
+		Text Domain: aaps
+	*/
+
+	/******************************************************
+	** Install
+	******************************************************/
+
+	register_activation_hook(__FILE__, function(){ });
+
+
+	/******************************************************
+	** Uninstall
+	******************************************************/
+
+	register_deactivation_hook(__FILE__, function(){ });
+	
+
+	/******************************************************
+	** Styles ans Scripts
+	******************************************************/
+
+	add_action('admin_enqueue_scripts', function() {
+		wp_enqueue_style('aaps_style_css', plugins_url('/css/style.css', __FILE__ ), null, '1.0.0');
+		
+        wp_enqueue_script('aaps_scripts_app', plugin_dir_url(__FILE__) . '/js/app.js', array(), '1.0.0', true);
+        wp_enqueue_script('aaps_scripts_sortable', plugin_dir_url(__FILE__) . '/vendor/jquery-sortable.js', array(), '0.9.13', true);
+	});
+	
+
+	/******************************************************
+	** Register Option/Setting
+	******************************************************/
+
+	add_action('admin_init', function () {
+		add_option('appartig_page_select_value', '[]');
+		register_setting('appartig_page_select', 'appartig_page_select_value', function($input) {
+			return $input;
+		});
+	});
+
+
+	/******************************************************
+	** Menu
+	******************************************************/
+
+    add_action('admin_menu', function() {
+
+		add_menu_page(
+			__("Page Select", "aaps"),
+			__("Page Select", "aaps"),
+			'manage_options',
+			'appartig_page_select',
+			'appartig_page_select__html',
+			'dashicons-list-view',
+			80
+		);
+	});
+
+
+	/******************************************************
+	** Admin Page
+	******************************************************/
+
+	function appartig_page_select__html(){
+
+		$json = get_option('appartig_page_select_value');
+		$pages = get_pages();
+
+		if (!isset($_REQUEST['settings-updated']))$_REQUEST['settings-updated'] = false;	
+		if ( false !== $_REQUEST['settings-updated'] ) : 
+	?> 
+		<div class="updated fade">
+			<p><strong>Einstellungen gespeichert!</strong></p>
+		</div>
+		
+	<?php endif; ?>
+
+		<div class="aaps wrap">
+	
+			<form method="post" action="options.php">
+
+				<?php 
+					settings_fields( 'appartig_page_select' );
+				?>
+			
+				<input type="hidden" id="appartig_page_select_value" name="appartig_page_select_value" value='<?php echo $json; ?>'>
+				<h1 class="wp-heading-inline">Page Select</h1>	
+				<button type="submit" class="page-title-action">Speichern</button>
+			</form>
+
+			<hr class="wp-header-end">
+
+			<ol class="aaps-list aaps-list--not-ready">
+
+				<?php
+					foreach($pages as $page){
+
+						echo "<li class='aaps-item' data-id='{$page->ID}'>";
+						echo "{$page->post_title}";
+						echo "<div class='aaps-item__show'><label  for='aaps-item__show--{$page->ID}'>Auf Startseite zeigen</label><input id='aaps-item__show--{$page->ID}' type='checkbox' /></div>";
+						echo "</li>";
+
+					}
+				?>
+
+			</ol>
+		</div>
+
+		<?php
+
+	}
+	
+
+	/******************************************************
+	** receive selected Posts
+	******************************************************/
+
+	function appartigPageSelect (){
+		
+		$allPages = get_pages();
+		$pages = [];
+		$selectedObject = json_decode(get_option('appartig_page_select_value'));
+		$selcted = [];
+
+		foreach($selectedObject as $orderdPage){
+			if($orderdPage->checked) {
+				foreach($allPages as $page){
+					if ($page->ID == $orderdPage->id) $pages[] = $page;
+				}
+			}
+		}
+		
+		return $pages;
+	}
+
+
---
+++ js/app.js
@@ -0,0 +1,55 @@
+jQuery(document).ready(function(){
+	initSortable();
+	initJson();
+
+	jQuery("input[type=checkbox]").change(() => createJson());
+});
+
+function initSortable(){
+	jQuery(".aaps-list").sortable({
+		onDrop: function ($item, container, _super, event) {
+			$item.removeClass(container.group.options.draggedClass).removeAttr("style")
+			jQuery("body").removeClass(container.group.options.bodyClass)
+			createJson();
+		}
+	});
+}
+
+function initJson(){
+	let jsonString = jQuery('#appartig_page_select_value').val();
+	let json = JSON.parse(jsonString);
+
+	// Init if not exist
+	if (json.length == 0){
+		createJson();
+	}
+
+	// Order Items
+	let newHTML = jQuery('<ol class="aaps-list"></ol>');
+	
+	json.forEach(el => {
+		let item = jQuery(".aaps-list li.aaps-item[data-id='" + el.id + "']");
+		if(el.checked) jQuery("#aaps-item__show--" + el.id).attr("checked", "checked");
+		newHTML.append(item);
+	});
+
+	jQuery(".aaps-list").remove();
+	jQuery(".aaps").append(newHTML);
+	initSortable();
+}
+
+function createJson(){
+	
+	let array = [];
+
+	jQuery(".aaps-list li.aaps-item").each(function(i, el){
+		let id = jQuery(el).data('id');
+		let item = {
+			id: id,
+			checked: jQuery("#aaps-item__show--" + id).prop('checked') ? true : false
+		};
+		array.push(item);
+	});
+
+	jQuery('#appartig_page_select_value').val(JSON.stringify(array));
+}
